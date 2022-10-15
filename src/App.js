3aadd84a2a13373666c79f5c043f988a0ee87da8
manisha sinha <bits.manisha@gmail.com>
2022-10-15 22:55:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+export default function App() {
+  const [resourceType, setResourceType] = useState('posts')
+  return (
+    <>
+    <div>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+    </div>
+    <h1>{resourceType}</h1>
+    </>
+  )
+}
+
+;
