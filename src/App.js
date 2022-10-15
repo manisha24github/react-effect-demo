c69494894f3e346e176c5f483ac6310c1874f90a
manisha sinha <bits.manisha@gmail.com>
2022-10-15 23:47:49 +0200

    Resize the window width.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import React, { useState, useEffect } from 'react';
 
 
 export default function App() {
-  const [resourceType, setResourceType] = useState('posts')
-  console.log('render')
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+
+const handleResize=() => {
+setwindowWidth(window.innerWidth)
+}
 
   useEffect(() =>{
-    console.log('resource type changed')
-  }, [resourceType])
+    window.addEventListener('resize', handleResize)
+    return() =>{
+      window.removeEventListener('resize', handleResize)
+    }
+  },[])
+
   return (
-    <>
-    <div>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-    </div>
-    <h1>{resourceType}</h1>
-    </>
+  
+    <div>{windowWidth}</div>
   )
 }
 
